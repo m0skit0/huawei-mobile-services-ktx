96db7298d77f626e1d99359ea8c70c20ec586fc9
m0skit0 <ametric@gmail.com>
2020-04-04 23:40:01 +0200

    Added core dependencies

---
+++ core/src/main/java/org/m0skit0/android/huawei/core/Initialization.kt
@@ -0,0 +1,8 @@
+package org.m0skit0.android.huawei.core
+
+import android.app.Application
+import org.m0skit0.android.huawei.core.di.loadKoin
+
+fun Application.loadHuaweiMobileServices() {
+    loadKoin()
+}

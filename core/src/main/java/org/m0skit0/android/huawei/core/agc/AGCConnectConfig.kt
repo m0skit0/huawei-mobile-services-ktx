3018f8f30a345f8e9c970ef8b038fbe8f4a285f0
m0skit0 <ametric@gmail.com>
2020-04-04 23:40:48 +0200

    Added AGCConnectConfig file for functions

---
+++ core/src/main/java/org/m0skit0/android/huawei/core/agc/AGCConnectConfig.kt
@@ -0,0 +1,6 @@
+package org.m0skit0.android.huawei.core.agc
+
+import org.m0skit0.android.huawei.core.di.AGCModuleProvider.NAMED_HUAWEI_APP_ID
+import org.m0skit0.android.huawei.core.di.koin
+
+fun huaweiAppId(): String = koin().get(NAMED_HUAWEI_APP_ID)

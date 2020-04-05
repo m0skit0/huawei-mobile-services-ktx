858be4d98f100ea1ec04722463fb426464636906
m0skit0 <ametric@gmail.com>
2020-04-05 23:48:49 +0200

    Push - Returning Either instead of Option

--- push/src/main/java/org/m0skit0/android/huawei/push/Push.kt
+++ push/src/main/java/org/m0skit0/android/huawei/push/Push.kt
@@ -1,7 +1,6 @@
 package org.m0skit0.android.huawei.push
 
 import arrow.core.Either
-import arrow.core.Option
 import com.huawei.hms.push.HmsMessaging
 import org.m0skit0.android.huawei.core.hms.huaweiDeregisterHMSToken
 import org.m0skit0.android.huawei.core.hms.huaweiDeregisterHMSTokenMaybe
@@ -16,12 +15,18 @@ private const val TYPE = "HCM"
 suspend fun huaweiPushToken(): String = huaweiHMSToken(TYPE)
 suspend fun huaweiPushTokenMaybe(): Either<Throwable, String> = huaweiHMSTokenMaybe(TYPE)
 
-suspend fun huaweiDeregisterPushToken(token: String): Unit = huaweiDeregisterHMSToken(token, TYPE)
-suspend fun huaweiDeregisterPushTokenMaybe(token: String): Option<Throwable> = huaweiDeregisterHMSTokenMaybe(token, TYPE)
+suspend fun huaweiDeregisterPushToken(token: String): String = huaweiDeregisterHMSToken(token, TYPE)
 
-suspend fun huaweiTurnOnPush(): Unit = koin().get<HmsMessaging>().turnOnPush().suspendCoroutineUntilCompletion().toUnit()
-suspend fun huaweriTurnOnPushMaybe(): Option<Throwable> = Either.catch { huaweiTurnOnPush() }.swap().toOption()
+suspend fun huaweiDeregisterPushTokenMaybe(token: String): Either<Throwable, String> =
+    huaweiDeregisterHMSTokenMaybe(token, TYPE)
 
-suspend fun huaweiTurnOffPush(): Unit = koin().get<HmsMessaging>().turnOffPush().suspendCoroutineUntilCompletion().toUnit()
-suspend fun huaweiTurnOffPushMaybe(): Option<Throwable> = Either.catch { huaweiTurnOffPush() }.swap().toOption()
+suspend fun huaweiTurnOnPush(): Unit =
+    koin().get<HmsMessaging>().turnOnPush().suspendCoroutineUntilCompletion().toUnit()
+
+suspend fun huaweriTurnOnPushMaybe(): Either<Throwable, Unit> = Either.catch { huaweiTurnOnPush() }
+
+suspend fun huaweiTurnOffPush(): Unit =
+    koin().get<HmsMessaging>().turnOffPush().suspendCoroutineUntilCompletion().toUnit()
+
+suspend fun huaweiTurnOffPushMaybe(): Either<Throwable, Unit> = Either.catch { huaweiTurnOffPush() }
 

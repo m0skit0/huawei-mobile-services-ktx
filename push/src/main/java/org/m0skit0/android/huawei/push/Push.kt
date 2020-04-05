a390cad8ef7cf4bc580075295844ec9b90f9412d
m0skit0 <ametric@gmail.com>
2020-04-05 12:34:42 +0200

    Push - Added token deregistering

--- push/src/main/java/org/m0skit0/android/huawei/push/Push.kt
+++ push/src/main/java/org/m0skit0/android/huawei/push/Push.kt
@@ -1,5 +1,16 @@
 package org.m0skit0.android.huawei.push
 
+import arrow.core.Either
+import arrow.core.Option
+import org.m0skit0.android.huawei.core.hms.huaweiDeregisterHMSToken
+import org.m0skit0.android.huawei.core.hms.huaweiDeregisterHMSTokenMaybe
 import org.m0skit0.android.huawei.core.hms.huaweiHMSToken
+import org.m0skit0.android.huawei.core.hms.huaweiHMSTokenMaybe
 
-suspend fun huaweiPushToken(): String = huaweiHMSToken("HCM")
+private const val TYPE = "HCM"
+
+suspend fun huaweiPushToken(): String = huaweiHMSToken(TYPE)
+suspend fun huaweiPushTokenMaybe(): Either<Throwable, String> = huaweiHMSTokenMaybe(TYPE)
+
+suspend fun huaweiDeregisterPushToken(token: String): Unit = huaweiDeregisterHMSToken(token, TYPE)
+suspend fun huaweiDeregisterPushTokenMaybe(token: String): Option<Throwable> = huaweiDeregisterHMSTokenMaybe(token, TYPE)

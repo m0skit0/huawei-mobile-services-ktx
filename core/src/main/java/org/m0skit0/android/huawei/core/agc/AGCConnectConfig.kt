46da2afbe2163c09f967b1bf78ce150ea77f8895
m0skit0 <ametric@gmail.com>
2020-04-05 12:18:58 +0200

    Core - Added "maybe" versions for functions (returns Either)

--- core/src/main/java/org/m0skit0/android/huawei/core/agc/AGCConnectConfig.kt
+++ core/src/main/java/org/m0skit0/android/huawei/core/agc/AGCConnectConfig.kt
@@ -1,6 +1,8 @@
 package org.m0skit0.android.huawei.core.agc
 
+import arrow.core.Either
 import org.m0skit0.android.huawei.core.di.AGCModuleProvider.NAMED_HUAWEI_APP_ID
 import org.m0skit0.android.huawei.core.di.koin
 
 suspend fun huaweiAppId(): String = koin().get(NAMED_HUAWEI_APP_ID)
+suspend fun huaweiAppIdMaybe(): Either<Throwable, String> = Either.catch { huaweiAppId() }
--- core/src/main/java/org/m0skit0/android/huawei/core/hms/HMS.kt
+++ core/src/main/java/org/m0skit0/android/huawei/core/hms/HMS.kt
@@ -1,5 +1,6 @@
 package org.m0skit0.android.huawei.core.hms
 
+import arrow.core.Either
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.m0skit0.android.huawei.core.di.HMSModuleProvider.NAMED_HUAWEI_TOKEN
@@ -10,6 +11,15 @@ import org.m0skit0.android.huawei.core.di.koin
  * EMUI < 10.0 uses callback for the token.
  * See https://developer.huawei.com/consumer/en/doc/development/HMS-Guides/push-basic-capability
  */
-suspend fun huaweiHMSToken(type: String): String = withContext(Dispatchers.Main) {
-    koin().get<(String) -> String>(NAMED_HUAWEI_TOKEN)(type)
-}
+suspend fun huaweiHMSToken(type: String): String =
+    withContext(Dispatchers.Main) {
+        koin().get<(String) -> String>(NAMED_HUAWEI_TOKEN)(type)
+    }
+
+/**
+ * For now this only supports EMUI 10.0 or higher.
+ * EMUI < 10.0 uses callback for the token.
+ * See https://developer.huawei.com/consumer/en/doc/development/HMS-Guides/push-basic-capability
+ */
+suspend fun huaweiHMSTokenMaybe(type: String): Either<Throwable, String> =
+    Either.catch { huaweiHMSToken(type) }

27f324adcf1d095ba8eb5c901b38d82fd82f754a
m0skit0 <ametric@gmail.com>
2021-05-26 19:31:53 +0200

    Added push module and removed all code on it

--- push/src/main/java/org/m0skit0/android/huawei/push/HuaweiPushMessageReceiver.kt
+++
@@ -1,8 +0,0 @@
-package org.m0skit0.android.huawei.push
-
-import com.huawei.hms.push.RemoteMessage
-
-interface HuaweiPushMessageReceiver {
-    fun onHuaweiPushMessageReceived(remoteMessage: RemoteMessage)
-    fun onHuaweiNewTokenReceived(token: String)
-}
